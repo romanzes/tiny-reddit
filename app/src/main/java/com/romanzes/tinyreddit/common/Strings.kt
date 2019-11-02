43f2eb9325f172f09b91f5b7a25b1c5f072f784a
Roman Petrenko <roman@canva.com>
2019-11-03 09:39:14 +1100

    Removed Optional and fixed warnings in Strings

--- app/src/main/java/com/romanzes/tinyreddit/common/Strings.kt
+++ app/src/main/java/com/romanzes/tinyreddit/common/Strings.kt
@@ -3,11 +3,11 @@ package com.romanzes.tinyreddit.common
 import android.content.Context
 import com.romanzes.tinyreddit.R
 
-class Strings(val context: Context) {
-    fun generalErrorText() = context.getString(R.string.general_error)
+class Strings(private val context: Context) {
+    fun generalErrorText(): String = context.getString(R.string.general_error)
 
-    fun author(author: String) = context.getString(R.string.author_pattern, author)
+    fun author(author: String): String = context.getString(R.string.author_pattern, author)
 
-    fun subreddit(subreddit: String) =
+    fun subreddit(subreddit: String): String =
         context.getString(R.string.subreddit_pattern, subreddit)
 }

2a3bcfc2a76dbe481452c797c488014071905277
Roman Petrenko <roman@canva.com>
2019-11-03 15:07:53 +1100

    Use Kotlin bindings on ViewHolder

--- app/src/main/java/com/romanzes/tinyreddit/ui/posts/PostsAdapter.kt
+++ app/src/main/java/com/romanzes/tinyreddit/ui/posts/PostsAdapter.kt
@@ -3,13 +3,12 @@ package com.romanzes.tinyreddit.ui.posts
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.romanzes.tinyreddit.R
-import com.romanzes.tinyreddit.model.Post
 import com.romanzes.tinyreddit.ext.show
+import com.romanzes.tinyreddit.model.Post
+import kotlinx.android.synthetic.main.item_post.view.*
 
 class PostsAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostViewHolder>() {
     var onItemClicked: ((Post) -> Unit)? = null
@@ -17,24 +16,7 @@ class PostsAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostVie
     override fun getItemCount(): Int = posts.size
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        val post = posts[position]
-        holder.apply {
-            onItemClicked?.let { listener ->
-                itemView.setOnClickListener {
-                    listener(post)
-                }
-            }
-            title.text = post.title
-            author.text = post.author
-            subreddit.text = post.subreddit
-            preview.show(post.previewLink != null)
-            post.previewLink?.let { link ->
-                Glide
-                    .with(preview.context)
-                    .load(link)
-                    .into(preview)
-            }
-        }
+        holder.bind(posts[position], onItemClicked)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder =
@@ -48,8 +30,21 @@ class PostsAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostVie
 }
 
 class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val title: TextView = itemView.findViewById(R.id.title)
-    val author: TextView = itemView.findViewById(R.id.author)
-    val subreddit: TextView = itemView.findViewById(R.id.subreddit)
-    val preview: ImageView = itemView.findViewById(R.id.preview)
+    fun bind(post: Post, onItemClicked: ((Post) -> Unit)?) {
+        onItemClicked?.let { listener ->
+            itemView.setOnClickListener {
+                listener(post)
+            }
+        }
+        itemView.title.text = post.title
+        itemView.author.text = post.author
+        itemView.subreddit.text = post.subreddit
+        itemView.preview.show(post.previewLink != null)
+        post.previewLink?.let { link ->
+            Glide
+                .with(itemView.context)
+                .load(link)
+                .into(itemView.preview)
+        }
+    }
 }
